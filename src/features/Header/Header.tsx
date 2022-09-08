db3927d09d5fbd810cc94fbcfabcf74578420dd7
nixjke <aziz.10.255@gmail.com>
2022-09-08 22:48:44 +0300

    add header

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,15 +1,69 @@
-import { Container as MuiContainer, styled, Typography } from '@mui/material'
+import { Container as MuiContainer, styled, Typography, Box } from '@mui/material'
 import React from 'react'
+import { colors } from 'theme'
 
-const Container = styled(MuiContainer)(() => ({
-  backgroundColor: '#005BD1',
+const ContainerWrapper = styled(MuiContainer)(() => ({
+  backgroundColor: colors.blue,
   height: '588px',
+  paddingTop: 38
+}))
+
+const NavLink = styled(Typography)(() => ({
+  backgroundColor: colors.blue,
+  display: 'flex',
+  color: '#fff',
+  position: 'relative',
+  cursor: 'pointer',
+  '&:after': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    right: 0,
+    bottom: '-3px',
+    width: 0,
+    height: '2px',
+    backgroundColor: '#fff',
+    transition: 'width 500ms',
+  },
+  '&:hover:after': {
+    content: '""',
+    width: '100%',
+    display: 'block',
+    position: 'absolute',
+    left: 0,
+    bottom: '-3px',
+    height: '2px',
+    backgroundColor: '#fff',
+    transition: 'width 500ms',
+  },
 }))
 
 export const Header: React.FC = () => {
   return (
-    <Container>
-      <Typography>Hello world</Typography>
-    </Container>
+    <ContainerWrapper maxWidth={false}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          maxWidth: '1440px',
+          padding: '0 16px',
+          margin: '0 auto',
+        }}
+      >
+        <Typography
+          sx={{ p: '20px 30px', backgroundColor: '#fff', borderRadius: '20px', fontSize: 26, color: colors.blue }}
+          variant="body1"
+        >
+          Логотип
+        </Typography>
+        <Box sx={{ display: 'flex', gap: '100px' }}>
+          <NavLink variant="body1">О сервисе</NavLink>
+          <NavLink>Документы</NavLink>
+          <NavLink>Контакты</NavLink>
+          <NavLink>Блог</NavLink>
+        </Box>
+      </Box>
+    </ContainerWrapper>
   )
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,5 +3,33 @@ import { createTheme } from '@mui/material'
 export const theme = createTheme({
   typography: {
     fontFamily: 'Montserrat',
+    h1: {
+      fontWeight: 700,
+      fontSize: '54px',
+      color: '#051A47',
+    },
+    h2: {
+      fontWeight: 700,
+      fontSize: '36px',
+      color: '#051A47',
+    },
+    h3: {
+      fontWeight: 700,
+      fontSize: '22px',
+      color: '#051A47',
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: '22px',
+      color: '#051A47',
+    },
   },
 })
+
+export const colors = {
+  indigo: '#051A47',
+  blue: '#005BD1',
+  orange: '#FDA059',
+  yellow: '#F3F462',
+  grey: '#DBE4F7',
+}
