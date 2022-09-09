8ff0bb5c585c524800a366bbe4c9ad4759f33bca
nixjke <aziz.10.255@gmail.com>
2022-09-09 14:35:11 +0300

    add company name

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material'
 import { Header } from 'features/Header/Header'
+import { Layout } from 'features/Layout/Layout'
 import React from 'react'
 
 export const Main: React.FC = () => {
   return (
     <>
       <Header />
+      <Layout />
     </>
   )
 }
