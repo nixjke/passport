f1988ad8ebb3d9bbc2977ac7cc2a74f63622703e
nixjke <aziz.10.255@gmail.com>
2022-09-09 15:46:31 +0300

    add modal

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,5 +1,4 @@
-import { Container as MuiContainer, styled, Typography, Box, Button } from '@mui/material'
-import { borderRadius } from '@mui/system'
+import { Container as MuiContainer, styled, Typography, Box, Button, Modal, Select, MenuItem } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import { colors } from 'theme'
@@ -43,82 +42,166 @@ const NavLink = styled(Typography)(() => ({
 }))
 
 export const Header: React.FC = () => {
+  const [open, setOpen] = React.useState<boolean>(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const [document, setDocument] = React.useState(0)
+
+  const handleChange = (e: any) => {
+    setDocument(e.target.value)
+  }
+
+  const uploadHandler = (e: any) => {
+    console.log(e.target.files[0])
+  }
+
   return (
-    <ContainerWrapper maxWidth={false}>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          maxWidth: '1440px',
-          padding: '0 16px',
-          margin: '0 auto',
-        }}
-      >
-        <Typography
-          sx={{ p: '20px 30px', backgroundColor: '#fff', borderRadius: '20px', fontSize: 26, color: colors.blue }}
-          variant="body1"
+    <>
+      <ContainerWrapper maxWidth={false}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            maxWidth: '1440px',
+            padding: '0 16px',
+            margin: '0 auto',
+          }}
         >
-          Логотип
-        </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '680px', gap: '4px' }}>
-          <NavLink>О сервисе</NavLink>
-          <NavLink>Документы</NavLink>
-          <NavLink>Контакты</NavLink>
-          <NavLink>Блог</NavLink>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          margin: '80px 140px 0 140px',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Box>
           <Typography
-            sx={{
-              color: '#fff',
-              width: 'max(660px)',
-            }}
-            variant="h1"
+            sx={{ p: '20px 30px', backgroundColor: '#fff', borderRadius: '20px', fontSize: 26, color: colors.blue }}
+            variant="body1"
           >
-            Поможем вам сделать качественное фото для документов
+            Логотип
           </Typography>
-          <Typography
-            sx={{
-              color: '#fff',
-              fontSize: '28px',
-            }}
-          >
-            с учетом размерных требований
-          </Typography>
-          <Button
-            sx={{
-              color: '#000',
-              borderRadius: '58px',
-              padding: '21px 40px',
-              backgroundColor: colors.yellow,
-              mt: '30px',
-              '&:hover': {
-                backgroundColor: '#fff',
-              },
-            }}
-          >
-            <Typography sx={{ fontSize: '18px', fontWeight: 700 }}>Загрузить фото</Typography>
-          </Button>
-        </Box>
-        <Box>
-          <Box
-            sx={{
-              width: '578px',
-              marginTop: '-66px',
-            }}
-          >
-            <Image placeholder="blur" src={headerImg} alt="" />
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '680px', gap: '4px' }}>
+            <NavLink>О сервисе</NavLink>
+            <NavLink>Документы</NavLink>
+            <NavLink>Контакты</NavLink>
+            <NavLink>Блог</NavLink>
           </Box>
         </Box>
-      </Box>
-    </ContainerWrapper>
+        <Box
+          sx={{
+            margin: '80px 140px 0 140px',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Box>
+            <Typography
+              sx={{
+                color: '#fff',
+                width: 'max(660px)',
+              }}
+              variant="h1"
+            >
+              Поможем вам сделать качественное фото для документов
+            </Typography>
+            <Typography
+              sx={{
+                color: '#fff',
+                fontSize: '28px',
+              }}
+            >
+              с учетом размерных требований
+            </Typography>
+            <Button
+              sx={{
+                color: '#000',
+                borderRadius: '58px',
+                padding: '21px 40px',
+                backgroundColor: colors.yellow,
+                mt: '30px',
+                '&:hover': {
+                  backgroundColor: '#fff',
+                },
+              }}
+              onClick={handleOpen}
+            >
+              <Typography sx={{ fontSize: '18px', fontWeight: 700 }}>Загрузить фото</Typography>
+            </Button>
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                width: '578px',
+                marginTop: '-66px',
+              }}
+            >
+              <Image placeholder="blur" src={headerImg} alt="" />
+            </Box>
+          </Box>
+        </Box>
+      </ContainerWrapper>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 'min(1072px, 100%)',
+            backgroundColor: '#fff',
+            boxShadow: '2px 4px 12px rgba(0, 0, 0, 0.25)',
+            borderRadius: '15px',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Typography
+            sx={{
+              margin: '60px 220px 0px',
+              '@media(max-width: 1060px)': {
+                margin: '60px 20px 0px',
+              },
+            }}
+            noWrap
+            variant="h1"
+          >
+            Выберите документ
+          </Typography>
+
+          <Select
+            sx={{
+              margin: '50px 220px 0px',
+              '@media(max-width: 1060px)': {
+                margin: '50px 20px 0px',
+              },
+            }}
+            value={document}
+            onChange={handleChange}
+            displayEmpty
+          >
+            <MenuItem value={0}>Item 1</MenuItem>
+            <MenuItem value={1}>Item 2</MenuItem>
+            <MenuItem value={2}>Item 3</MenuItem>
+            <MenuItem value={3}>Item 4</MenuItem>
+          </Select>
+          <Button
+            component="label"
+            onChange={uploadHandler}
+            sx={{
+              color: colors.orange,
+              borderRadius: '58px',
+              padding: '21px 40px',
+              backgroundColor: '#fff',
+              border: `1px solid ${colors.orange}`,
+              margin: '0 auto',
+              mt: '30px',
+              mb: '50px',
+              width: 'max(260px)',
+            }}
+          >
+            <Typography noWrap sx={{ fontSize: '18px', fontWeight: 700, color: colors.orange }}>
+              Загрузить фото
+            </Typography>
+            <input hidden type="file" />
+          </Button>
+        </Box>
+      </Modal>
+    </>
   )
 }
